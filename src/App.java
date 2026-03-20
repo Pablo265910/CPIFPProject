ddafbb06d233f4db5d30d559e33e9695c1575739
Pablo265910 <pagutor2510@g.educaand.es>
2026-03-20 19:52:43 +0100

    Actividad resuelta con las especificaciones indicadas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CPIFP cpif = new CPIFP("Alan Turing", "Calle Frederick Terman", "123456789");
+        Estudiante estudiante1 = new Estudiante("Pepe", "1A");
+        Estudiante estudiante2 = new Estudiante("Juan", "2B");
+        Estudiante estudiante3 = new Estudiante("María", "3C");
+
+        cpif.añadirEstudiante(estudiante1);
+        cpif.añadirEstudiante(estudiante2);
+        cpif.añadirEstudiante(estudiante3);
+
+        Departamento departamento1 = new Departamento("Departamento de matemáticas");
+        Departamento departamento2 = new Departamento("Departamento de programación");
+        Profesor profesor1 = new Profesor("Juan", "Matemáticas");
+        Profesor profesor2 = new Profesor("Julia", "Programación");
+        Profesor profesor3 = new Profesor("Carmen", "Matemáticas");
+        Profesor profesor4 = new Profesor("Ismael", "Programación");
+        
+        departamento1.añadirProfesor(profesor1);
+        departamento1.añadirProfesor(profesor3);
+        departamento1.setJefe(profesor1);
+
+        departamento2.añadirProfesor(profesor2);
+        departamento2.añadirProfesor(profesor4);
+        departamento2.setJefe(profesor4);
+        
+        cpif.añadirDepartamento(departamento1);
+        cpif.añadirDepartamento(departamento2);
+        
+        System.out.println(cpif);
+        System.out.println(cpif.getListaDepartamento());
+        System.out.println(cpif.getListaEstudiante());
     }
 }
--- src/CPIFP.java
+++ src/CPIFP.java
@@ -1,45 +1,78 @@
 import java.util.ArrayList;
 import java.util.List;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-
+@objid ("ab338505-81c2-495d-a73b-3704f230b6e8")
 public class CPIFP {
+    @objid ("41ee2652-a7c3-4629-81c5-dbfd63cb905b")
     private String nombre;
+
+    @objid ("f9238422-20af-43ee-8d39-156ffd9be39b")
     private String direccion;
+
+    @objid ("0c6bd6db-5eb9-45ab-b2b7-df265ca6d660")
     private String telefono;
+
+    @objid ("d4a6bc93-99f9-4ed8-bf67-6e62bec1b9fc")
     private List<Estudiante> listaEstudiantes = new ArrayList<Estudiante> ();
+
+    @objid ("54adfb3b-1054-4097-99cb-feaba53e96c6")
     private List<Departamento> listaDepartamentos = new ArrayList<Departamento> ();
 
-    
+    @objid ("27203417-66ff-4c01-941b-13793e4bb67f")
     public void añadirEstudiante(final Estudiante Estudiante) {
+        this.listaEstudiantes.add(Estudiante);
     }
 
+    @objid ("2241061f-2851-4fbd-b403-d1911e842156")
     public void quitarEstudiante(final Estudiante estudiante) {
     }
 
+    @objid ("6c4f9a84-6a2b-4492-9158-9932f2c818bf")
     public void añadirDepartamento(final Departamento departamento) {
+        this.listaDepartamentos.add(departamento);
     }
 
+    @objid ("584c7af5-d7e0-404f-a1df-63cbe6aa1ed6")
     public void quitarDepartamento(final Departamento Departamento) {
     }
 
+    @objid ("c8c9e282-c4aa-4ec8-92b5-c2aa5734f79f")
     public Estudiante getEstudiante(final String nombre) {
         // TODO Auto-generated return
         return null;
     }
 
+    @objid ("5d753d77-d368-489b-814e-0a07f755120a")
     public Departamento getDepartamento(final String nombre) {
         // TODO Auto-generated return
         return null;
     }
 
+    @objid ("facfc82a-2ebc-45ec-94da-19e7f7334a5c")
     public List<Departamento> getListaDepartamento() {
+        System.out.println("LISTA DE DEPARTAMENTOS");
         // TODO Auto-generated return
-        return null;
+        return listaDepartamentos;
     }
 
+    @objid ("fd91e1a6-bde6-4de9-a9de-89b31dc53c29")
     public List<Estudiante> getListaEstudiante() {
+        System.out.println("LISTA DE ESTUDIANTES");
         // TODO Auto-generated return
-        return null;
+        return listaEstudiantes;
+    }
+
+    @objid ("a727d9d4-0b26-4477-85e7-2b545e13c4e5")
+    public CPIFP(final String nombre, final String direccion, final String telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Nombre del centro: %s\nDireccion: %s\nTeléfono: %s\n", this.nombre, this.direccion,this.telefono);
     }
 
 }
